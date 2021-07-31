d036c796fc384a9a4ce23c24adf59a21d3898e3f
johnpjennings <52945770+johnpjennings@users.noreply.github.com>
2021-07-31 04:23:09 -0700

    Update bot.js

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,1 +1,22 @@
+const tmi = require('tmi.js');
 
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: 'my_bot_name',
+		password: 'oauth:my_bot_token'
+	},
+	channels: [ 'my_name' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message.toLowerCase() === '!hello') {
+		// "@alca, heya!"
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
